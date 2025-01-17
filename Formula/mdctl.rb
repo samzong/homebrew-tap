73711b513e989c1fe127ae4642dc46a182a80319
samzong <samzong.lu@gmail.com>
2025-01-17 15:23:44 +0800

    fix: update mdctl release file names
    Signed-off-by: samzong <samzong.lu@gmail.com>

--- Formula/mdctl.rb
+++ Formula/mdctl.rb
@@ -5,20 +5,20 @@ class Mdctl < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/samzong/mdctl/releases/download/v#{version}/mdctl_#{version}_darwin_arm64.tar.gz"
+      url "https://github.com/samzong/mdctl/releases/download/v#{version}/mdctl_Darwin_arm64.tar.gz"
       sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     else
-      url "https://github.com/samzong/mdctl/releases/download/v#{version}/mdctl_#{version}_darwin_amd64.tar.gz"
+      url "https://github.com/samzong/mdctl/releases/download/v#{version}/mdctl_Darwin_x86_64.tar.gz"
       sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/samzong/mdctl/releases/download/v#{version}/mdctl_#{version}_linux_arm64.tar.gz"
+      url "https://github.com/samzong/mdctl/releases/download/v#{version}/mdctl_Linux_arm64.tar.gz"
       sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     else
-      url "https://github.com/samzong/mdctl/releases/download/v#{version}/mdctl_#{version}_linux_amd64.tar.gz"
+      url "https://github.com/samzong/mdctl/releases/download/v#{version}/mdctl_Linux_x86_64.tar.gz"
       sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   end
