9697d4d25c3d15f664cedbf9a1a8ac9cea7d3bad
samzong <samzong.lu@gmail.com>
2025-04-03 18:40:50 +0800

    Update configforge.rb

--- Casks/configforge.rb
+++ Casks/configforge.rb
@@ -8,10 +8,10 @@ cask "configforge" do
   auto_updates true
 
   if Hardware::CPU.arm?
-    url "https://github.com/samzong/MacMusicPlayer/releases/download/v#{version}/ConfigForge-arm64.dmg"
+    url "https://github.com/samzong/ConfigForge/releases/download/v#{version}/ConfigForge-arm64.dmg"
     sha256 "d28900f29a753263675001e34246c4b58bb4676d49e475324b67fb0803cea775"
   else
-    url "https://github.com/samzong/MacMusicPlayer/releases/download/v#{version}/ConfigForge-x86_64.dmg"
+    url "https://github.com/samzong/ConfigForge/releases/download/v#{version}/ConfigForge-x86_64.dmg"
     sha256 "c5a03673b6d693eadb97368b5ebed04b468185b3939c0dceed8449d58ad70b2d"
   end
 
