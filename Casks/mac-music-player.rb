c44b797dfe98ecf16b0f7864fc6190e301084594
samzong <samzong.lu@gmail.com>
2025-05-15 13:51:37 +0800

    feat(cask): add macOS and formula dependencies to mac-music-player.rb

--- Casks/mac-music-player.rb
+++ Casks/mac-music-player.rb
@@ -1,12 +1,5 @@
 cask "mac-music-player" do
-  app "MacMusicPlayer.app"
   version "0.3.0"
-
-  name "MacMusicPlayer"
-  desc "A simple and elegant music player for macOS"
-  homepage "https://github.com/samzong/MacMusicPlayer"
-  auto_updates true
-
   if Hardware::CPU.arm?
     url "https://github.com/samzong/MacMusicPlayer/releases/download/v#{version}/MacMusicPlayer-arm64.dmg"
     sha256 "5257fd006d0bd6cc28d2f20587b49537d383bcd37d2c36877bb43d6184cc35f6"
@@ -14,6 +7,16 @@ cask "mac-music-player" do
     url "https://github.com/samzong/MacMusicPlayer/releases/download/v#{version}/MacMusicPlayer-x86_64.dmg"
     sha256 "2e696b339725941e026262f7bf9a98e3eac136d216fbba197ba56e8bcc3a7ddc"
   end
+
+  name "MacMusicPlayer"
+  desc "A simple and elegant music player for macOS"
+  homepage "https://github.com/samzong/MacMusicPlayer"
+  auto_updates true
+
+  depends_on macos: ">= :monterey"
+  depends_on formula: ["yt-dlp", "ffmpeg"]
+
+  app "MacMusicPlayer.app"
 
   zap trash: [
     "~/Library/Application Support/MacMusicPlayer",
