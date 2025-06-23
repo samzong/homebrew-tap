ef0aa80792ea6b6bc1a5e4e88d9c30335e8707b6
samzong <samzong.lu@gmail.com>
2025-06-23 13:43:29 +0800

    feat(casks): add SaveEye cask for minimalist eye care reminder app

---
+++ Casks/saveeye.rb
@@ -0,0 +1,44 @@
+cask "saveeye" do
+  version "1.0.3"
+
+  name "SaveEye"
+  desc "minimalist eye care reminder app"
+  homepage "https://github.com/samzong/SaveEye"
+
+  auto_updates true
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :big_sur"
+
+  on_arm do
+    url "https://github.com/samzong/SaveEye/releases/download/v#{version}/SaveEye-#{version}-arm64.dmg"
+    sha256 "54d5e61397c9c1759fe6d5e4ded627f3920a58cb9529b479c7de7f3fdc4c7682"
+  end
+
+  on_intel do
+    url "https://github.com/samzong/SaveEye/releases/download/v#{version}/SaveEye-#{version}-x86_64.dmg"
+    sha256 "54d5e61397c9c1759fe6d5e4ded627f3920a58cb9529b479c7de7f3fdc4c7682"
+  end
+
+  postflight do
+    system_command "xattr", args: ["-cr", "#{appdir}/SaveEye.app"]
+  end
+  
+  zap trash: [
+    "/Library/Logs/DiagnosticReports/SaveEye*",
+    "~/Library/Application Support/SaveEye",
+    "~/Library/Caches/com.samzong.saveeye",
+    "~/Library/Containers/com.samzong.saveeye",
+    "~/Library/Group Containers/group.com.samzong.saveeye",
+    "~/Library/HTTPStorages/com.samzong.saveeye",
+    "~/Library/LaunchAgents/com.samzong.saveeye.plist",
+    "~/Library/Logs/SaveEye",
+    "~/Library/Preferences/ByHost/com.samzong.saveeye.*.plist",
+    "~/Library/Preferences/com.samzong.saveeye.plist",
+    "~/Library/Saved Application State/com.samzong.saveeye.savedState",
+  ]
+end
