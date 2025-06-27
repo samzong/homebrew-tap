c90a98bcd5aa72f5ec44c03e90377d9998012a22
samzong <samzong.lu@gmail.com>
2025-06-28 00:06:28 +0800

    chore: bump to v0.0.1

--- Formula/mm.rb
+++ Formula/mm.rb
@@ -6,20 +6,20 @@ class mm < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/samzong/mm/releases/download/v#{version}/mm_Darwin_arm64.tar.gz"
-      sha256 "fcd4601e0528fe8f56b42fb168164d910fb50c144f41926fc2c819e0e002955a"
+      sha256 "e65bd09ec1be71e5a623d63986268b4ca7858532d6f461cac428c59e80191b96"
     else
       url "https://github.com/samzong/mm/releases/download/v#{version}/mm_Darwin_x86_64.tar.gz"
-      sha256 "1917168af4fa2c9ce980ef3311ab4cee9a1f2340a99988471e58dc7f4fe62753"
+      sha256 "a5062eac5c8dd74ab94b3c26627ba65474de83bdf95e8c121e4da69f5fe2a407"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/samzong/mm/releases/download/v#{version}/mm_Linux_arm64.tar.gz"
-      sha256 "7db79d6b996dec9afae977f13e9269371074b24e11633833101fda82bf62063a"
+      sha256 "239da37da5a0aaabe33fa0d135ae7391d8d3b01c14e122ffe5581702567f60db"
     else
       url "https://github.com/samzong/mm/releases/download/v#{version}/mm_Linux_x86_64.tar.gz"
-      sha256 "ed5415052e838d0b05f7d00b34b5bb6325f1a79cb218f566f39dda5366962bdc"
+      sha256 "6b2f48b4a9a188a8bef22af3e19822c47c57fc85f4dfd9719c84addf7d7e0138"
     end
   end
 
