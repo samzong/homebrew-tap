8855de52aeae91f7620a16d32afd4716d2638c72
Anka <runner@iad20-fj923-81670e48-fb74-4d26-99de-c19e6b059a4d-F2E05A8AC5A2.local>
2026-01-14 01:39:33 +0000

    chore: bump to v0.7.0

--- Formula/gmc.rb
+++ Formula/gmc.rb
@@ -1,25 +1,25 @@
 class Gmc < Formula
   desc "CLI for that accelerates the efficiency of Git add and commit"
   homepage "https://github.com/samzong/gmc"
-  version "0.6.2"
+  version "0.7.0"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/samzong/gmc/releases/download/v#{version}/gmc_Darwin_arm64.tar.gz"
-      sha256 "9982c900360face556c30dcf92a0b1b90ed6f64c174ec405c291994c5405f02c"
+      sha256 "bdc7c1b5cfb4f8b694c86758ff3fb39ea885fc9231f4243c4ff90aedc539ac82"
     else
       url "https://github.com/samzong/gmc/releases/download/v#{version}/gmc_Darwin_x86_64.tar.gz"
-      sha256 "5ff5c0acd86ffafce07da387a8872354c605c0272357bf9563c02ec0b2b083bd"
+      sha256 "f6b8703c18d450d23ab4183b2ab340e50130d38da4cfba7b2dbe720446850ecd"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/samzong/gmc/releases/download/v#{version}/gmc_Linux_arm64.tar.gz"
-      sha256 "cb775a585de3d58d1c22ddc9affae2ed98f5a4bba8e13d3c59436de58d65d57f"
+      sha256 "65d29f0bd8f8c0bed9813c0725286de735b3a19e90432097e56ff9de2658adf7"
     else
       url "https://github.com/samzong/gmc/releases/download/v#{version}/gmc_Linux_x86_64.tar.gz"
-      sha256 "7e1dc9e1d38a12ab0c3fedcf8f8c624dbcbb5881d2292f248857ae1682856d55"
+      sha256 "24931d63a9d0ddcfa9dcd4243a12f08f1a41b5f61984e58178c7a09d88a13087"
     end
   end
 
