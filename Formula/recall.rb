76fd708d9852bb14b6b17117802b13811e7ab078
samzong <samzong.lu@gmail.com>
2026-04-11 16:43:21 +0800

    chore: bump to v0.1.4
    Signed-off-by: samzong <samzong.lu@gmail.com>

--- Formula/recall.rb
+++ Formula/recall.rb
@@ -1,21 +1,21 @@
 class Recall < Formula
   desc "Local-first TUI for searching AI coding session history"
   homepage "https://github.com/samzong/Recall"
-  version "0.1.3"
+  version "0.1.4"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/samzong/Recall/releases/download/v#{version}/recall-macos-aarch64.tar.gz"
-      sha256 "94a15e0d5e940a24a57cfd84802aaa80f51fd77ae5ba59c60602d5bbccbc1884"
+      sha256 "3728c649e93dde3c24caa7144384f4b63162f8b20680fa9ca68e42af53d07e8f"
     else
       url "https://github.com/samzong/Recall/releases/download/v#{version}/recall-macos-x86_64.tar.gz"
-      sha256 "3eca099ef160961bae0205bc07e80e6a1e0dbc7a8e2edd23f7a6b633a95e3dbd"
+      sha256 "0d3bd00fb623c3b124a2b1e221f350495c7bca37b65f2d31d3b30aef75a9db15"
     end
   end
 
   on_linux do
     url "https://github.com/samzong/Recall/releases/download/v#{version}/recall-linux-x86_64.tar.gz"
-    sha256 "a9186a7e5b05c6ea7e0b4fa8e629807a8723094cdd1c6f1882bd8d52f5113493"
+    sha256 "27db5d3cab86a49dc0faa61e5acca940bb35cad8f740489af33af8e9ee70200c"
   end
 
   def install
